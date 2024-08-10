ee943998b46dd354559df67f602e88a167b14c7c
Timur Badretdinov <accounts@destiner.io>
2024-08-10 10:29:42 +0100

    Add AppHeader component

---
+++ src/components/_app/AppHeader.tsx
@@ -0,0 +1,17 @@
+import style from "./AppHeader.module.css";
+import ButtonWallet from "./header/ButtonWallet";
+
+const AppHeader: React.FC = () => {
+  return (
+    <header>
+      <div className={style.side}>
+        <a href="/">Home</a>
+      </div>
+      <div className={style.side}>
+        <ButtonWallet />
+      </div>
+    </header>
+  );
+};
+
+export default AppHeader;
