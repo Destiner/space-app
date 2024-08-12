7bf4de3eb06b1b12890a446b6f0e1e6382112006
Timur Badretdinov <accounts@destiner.io>
2024-08-12 16:43:35 +0100

    Fix endpoint url

--- src/app/space/[address]/page.tsx
+++ src/app/space/[address]/page.tsx
@@ -21,15 +21,15 @@ const Space: React.FC<Props> = ({ params }: Props) => {
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const address = params.address;
   console.log("address", address);
-  const url = `https://${process.env.VERCEL_URL}` || "http://localhost:3000";
+  const endpoint = process.env.VERCEL_URL
+    ? "https://space-app-scope-sh.vercel.app"
+    : "http://localhost:3000";
+  console.log("endpoint", endpoint);
+  const url = `${endpoint}/api/frame/space/${address}`;
   console.log("url", url);
-  const frameMetadata = await getFrameMetadata(
-    `${url}/api/frame/space/${address}`
-  );
+  const frameMetadata = await getFrameMetadata(url);
   console.log("frameMetadata", frameMetadata);
-  const text = await fetch(`${url}/api/frame/space/${address}`).then((r) =>
-    r.text()
-  );
+  const text = await fetch(url).then((r) => r.text());
   console.log("text", text);
   return {
     other: frameMetadata,
