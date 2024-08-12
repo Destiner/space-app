49c3e801ac8b6674017b8c64d051bb4c60e612df
Timur Badretdinov <accounts@destiner.io>
2024-08-12 14:42:24 +0100

    Rewrite frame

--- src/app/api/frame/[...routes]/route.tsx
+++ src/app/api/frame/[...routes]/route.tsx
@@ -1,76 +1,22 @@
 /** @jsxImportSource frog/jsx */
-import { Button, Frog } from "frog";
+import { Button, Frog, TextInput } from "frog";
 import { handle } from "frog/next";
-import { multicall } from "@wagmi/core";
-
-import spaceAbi from "@/abi/space";
-import { getConfig } from "@/wagmi";
-import { Address } from "viem";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
 
 const app = new Frog({
   basePath: "/api/frame",
-  // browserLocation: "/:path",
   title: "Frog Frame",
 });
 
-app.frame("/", async (c) => {
-  return c.res({
-    image: <div>Home</div>,
-  });
-});
-app.frame("/space/:address", async (c) => {
-  const spaceAddress = c.req.param("address") as Address;
-
-  const result = await multicall(getConfig(), {
-    contracts: [
-      {
-        abi: spaceAbi,
-        address: spaceAddress,
-        functionName: "name",
-      },
-      {
-        abi: spaceAbi,
-        address: spaceAddress,
-        functionName: "bio",
-      },
-      {
-        abi: spaceAbi,
-        address: spaceAddress,
-        functionName: "getLinks",
-        args: [0n, 100n],
-      },
-    ],
-  });
-
-  const nameResult = result[0];
-  const bioResult = result[1];
-  const linksResult = result[2];
-
-  if (
-    nameResult.status === "failure" ||
-    bioResult.status === "failure" ||
-    linksResult.status === "failure"
-  ) {
-    return c.res({
-      image: (
-        <div style="display:flex;flex-direction:column;color:white;font-size:60px;padding:20px;background:black;width:100%;height:100%;">
-          Unable to fetch the space
-        </div>
-      ),
-    });
-  }
-
-  const name = nameResult.result;
-  const bio = bioResult.result;
-  const links = linksResult.result;
-  console.log("space", name, bio, links);
-
+app.frame("/space/:address", (c) => {
+  const { buttonValue, status } = c;
   return c.res({
     image: (
-      <div style="display:flex;flex-direction:column;color:white;font-size:60px;padding:20px;background:black;width:100%;height:100%;">
-        <div style="margin-bottom:20px;">{name}</div>
-        <div style="margin-bottom:20px;">{bio}</div>
-        <div>{links}</div>
+      <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+        {status === "initial"
+          ? "Select your fruit!"
+          : `Selected: ${buttonValue}`}
       </div>
     ),
     intents: [
@@ -83,6 +29,8 @@ app.frame("/space/:address", async (c) => {
     ],
   });
 });
+
+devtools(app, { serveStatic });
 
 export const GET = handle(app);
 export const POST = handle(app);
