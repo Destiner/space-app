6789c026e1d24bffb9289d9dedcc3ee3bc895a95
Timur Badretdinov <accounts@destiner.io>
2024-08-12 16:45:25 +0100

    Clean up

--- src/app/space/[address]/page.tsx
+++ src/app/space/[address]/page.tsx
@@ -20,17 +20,11 @@ const Space: React.FC<Props> = ({ params }: Props) => {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const address = params.address;
-  console.log("address", address);
   const endpoint = process.env.VERCEL_URL
     ? "https://space-app-scope-sh.vercel.app"
     : "http://localhost:3000";
-  console.log("endpoint", endpoint);
   const url = `${endpoint}/api/frame/space/${address}`;
-  console.log("url", url);
   const frameMetadata = await getFrameMetadata(url);
-  console.log("frameMetadata", frameMetadata);
-  const text = await fetch(url).then((r) => r.text());
-  console.log("text", text);
   return {
     other: frameMetadata,
   };
