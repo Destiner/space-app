88470f92d1525332d740f6c92863803435926813
Timur Badretdinov <accounts@destiner.io>
2024-08-12 15:49:18 +0100

    Fetch space data

--- src/app/api/frame/[...routes]/route.tsx
+++ src/app/api/frame/[...routes]/route.tsx
@@ -1,8 +1,12 @@
 /** @jsxImportSource frog/jsx */
+import { getConfig } from "@/wagmi";
 import { Button, Frog } from "frog";
 import { handle } from "frog/next";
 import { createSystem } from "frog/ui";
 import { Address } from "viem";
+import { multicall } from "wagmi/actions";
+
+import spaceAbi from "@/abi/space";
 
 const { Box, Heading, Text, VStack } = createSystem();
 
@@ -27,8 +31,29 @@ const app = new Frog({
 
 export const runtime = "edge";
 
-app.frame("/space/:address", (c) => {
+app.frame("/space/:address", async (c) => {
   const spaceAddress = c.req.param("address") as Address;
+
+  const result = await multicall(getConfig(), {
+    contracts: [
+      {
+        abi: spaceAbi,
+        address: spaceAddress,
+        functionName: "name",
+      },
+      {
+        abi: spaceAbi,
+        address: spaceAddress,
+        functionName: "bio",
+      },
+      {
+        abi: spaceAbi,
+        address: spaceAddress,
+        functionName: "getLinks",
+        args: [0n, 100n],
+      },
+    ],
+  });
 
   const { buttonValue, status } = c;
   return c.res({
