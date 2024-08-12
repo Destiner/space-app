87bfaaa6e4b4b1e8cb27ba2cb8e9062c5bc777d7
Timur Badretdinov <accounts@destiner.io>
2024-08-12 16:22:19 +0100

    Generate frame metadata for space page

--- src/app/space/[address]/page.tsx
+++ src/app/space/[address]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { getFrameMetadata } from "frog/next";
+import type { Metadata } from "next";
+
 import React, { useState, useEffect, useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import request from "graphql-request";
@@ -475,5 +478,14 @@ const Space: React.FC<Props> = ({ params }: Props) => {
     </div>
   );
 };
+
+export async function generateMetadata(): Promise<Metadata> {
+  const url = process.env.VERCEL_URL || "http://localhost:3000";
+  console.log("url", url);
+  const frameMetadata = await getFrameMetadata(`${url}/api/frame`);
+  return {
+    other: frameMetadata,
+  };
+}
 
 export default Space;
